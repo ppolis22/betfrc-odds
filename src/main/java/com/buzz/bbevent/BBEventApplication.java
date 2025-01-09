cb71146f358d7534536c731239bedae1e968f2e3
Peter Polis <ppolis22@yahoo.com>
2025-01-09 00:40:01 -0500

    Initial commit

---
+++ src/main/java/com/buzz/bbevent/BBEventApplication.java
@@ -0,0 +1,13 @@
+package com.buzz.bbevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BBEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BBEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buzz/bbevent/BbeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buzz.bbevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
